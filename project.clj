11e03f4d98980fd4a31a60f4ddfa7eda5e99bb4e
Eric Shull <eric@exupero.org>
2024-11-19 08:43:45 -0500

    Convert from lein to deps.edn project

--- project.clj
+++
@@ -1,6 +0,0 @@
-(defproject org.exupero/svg "0.1.0-SNAPSHOT"
-  :description "SVG utils for Clojure"
-  :url "https://github.com/exupero/svg"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
